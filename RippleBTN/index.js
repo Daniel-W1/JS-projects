e08cff1fa80e35884b26fac830d204a11982c1f6
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-19 10:40:26 +0300

    Update index.js

--- RippleBTN/index.js
+++ RippleBTN/index.js
@@ -1,1 +1,10 @@
 
+const Button = document.querySelector(".btn");
+Button.addEventListener("mouseover", (event)=>{
+    const xpos = (event.pageX - Button.offsetLeft);
+    const ypos = (event.pageY - Button.offsetTop);
+
+    console.log(xpos, ypos)
+    Button.style.setProperty("--xPos", xpos+"px");
+    Button.style.setProperty("--yPos", ypos+"px");
+})
