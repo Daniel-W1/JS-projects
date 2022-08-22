8fd4200e9b17e816bc8b12e4ffd9194cbd9e78e0
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-22 11:47:57 +0300

    Update first.js

--- Heart Animation/first.js
+++ Heart Animation/first.js
@@ -1,1 +1,20 @@
 
+const body = document.querySelector("body");
+
+body.addEventListener("mousemove", (event)=>{
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    const newHeart = document.createElement("span");
+    newHeart.style.left = x + "px";
+    newHeart.style.top = y + "px";
+
+    const randomSize = Math.round(Math.random()*100);
+    newHeart.style.width = randomSize +"px";
+    newHeart.style.height = randomSize +"px";
+    body.appendChild(newHeart);
+
+    setTimeout(()=>{
+        newHeart.remove();
+    },4000)
+})
