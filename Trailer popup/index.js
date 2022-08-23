2b41b3fe344705152cac25ebbe03c22fd70c72f1
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-23 12:35:29 +0300

    Update index.js

--- Trailer popup/index.js
+++ Trailer popup/index.js
@@ -1,1 +1,15 @@
 
+const button = document.querySelector(".btn");
+const trailer = document.querySelector(".trailer");
+const video = document.querySelector("video");
+const close = document.querySelector(".close")
+
+console.log(button)
+button.addEventListener("click", ()=>{
+    trailer.classList.remove("inactive");
+})
+close.addEventListener("click", ()=>{
+    trailer.classList.add("inactive");
+    video.pause();
+    video.currentTime = 0;
+})
