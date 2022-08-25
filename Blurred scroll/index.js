6df9469761324747f4fd1d931d4af936ee05f4df
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-25 09:46:20 +0300

    Update index.js

--- Blurred scroll/index.js
+++ Blurred scroll/index.js
@@ -1,1 +1,7 @@
 
+const image = document.querySelector(".bg-image");
+
+window.addEventListener("scroll", ()=>{
+    image.style.opacity = 1 - window.pageYOffset/800;
+    image.style.backgroundSize = 160 - window.pageYOffset/15 + "%";
+})
